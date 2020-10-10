c423163d943725f3997caf8a6f9472659de82791
Max Williams <max@welcometomonday.com>
2020-10-09 19:55:39 -0700

    Added new core collections, CircularArray and Feed. Added several new helpful extension methods to Ladybug.Core. Updated ECS Submodule

---
+++ src/core/collections/CircularArray.cs
@@ -0,0 +1,56 @@
+namespace Ladybug.Core
+{
+	public class CircularArray<T>
+	{
+		private T[] _list;
+
+		private int _start;
+
+		public CircularArray(int capacity = 100)
+		{
+			_list = new T[capacity];
+		}
+
+		public T this[int i]
+		{
+			get => _list[(_start + i) % _list.Length];
+			set => _list[(_start + i) % _list.Length] = value;
+		}
+
+		public int Start
+		{
+			get => _start;
+			set => _start = value % _list.Length;
+		}
+
+		public int Count { get; set; }
+
+		public int Capacity { get => _list.Length; }
+
+		public void Populate<K>() where K : T, new()
+		{
+			for (var i = 0; i < Capacity; i++)
+			{
+				this[i] = new K();
+			}
+		}
+
+		public T Get()
+		{
+			T res = default(T);
+
+			if (Count == Capacity)
+			{
+				res = this[0];
+				Start++;
+			}
+			else
+			{
+				res = this[Count];
+				Count++;
+			}
+
+			return res;
+		}
+	}
+}
