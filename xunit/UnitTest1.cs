f4450ec3fe5a3027b5ad42f9cb78d09f1b075a86
Max Williams <max@welcometomonday.com>
2021-06-20 15:48:02 -0700

    Added xunit test project

---
+++ xunit/UnitTest1.cs
@@ -0,0 +1,30 @@
+using System;
+using Xunit;
+
+using Ladybug;
+
+namespace Ladybug.Test
+{
+	public class SampleTest
+	{
+		[Fact]
+		public void PassingTest()
+		{
+			Assert.Equal(4, Add(2, 2));
+		}
+		
+		[Fact]
+		public void FailingTest() => Assert.Equal(5, Add(2, 2));
+		
+		
+		[Theory]
+		[InlineData(3)]
+		[InlineData(5)]
+		[InlineData(6)]
+		public void FirstTheory(int value) => Assert.True(IsOdd(value));
+
+		private int Add(int x, int y) => x + y;
+
+		private bool IsOdd(int value) => value % 2 == 1;
+	}
+}
