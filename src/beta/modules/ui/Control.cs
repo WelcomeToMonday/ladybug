34b18a383a05bcc808522d3560dbce13221068ec
Max Williams <max@welcometomonday.com>
2021-07-09 23:37:27 -0700

    Fleshing out Control and UI 2.0

--- src/beta/modules/ui/Control.cs
+++ src/beta/modules/ui/Control.cs
@@ -1,33 +1,50 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Ladybug.Beta.UI
 {
 	public abstract class Control
 	{
+		public event EventHandler Click;
+		public event EventHandler Focus;
+		public event EventHandler Unfocus;
+
 		private int _zIndex = 0;
 		private List<Control> _children = new List<Control>();
 
+		private Action _onInitialize = () => { };
+		private Action<Control> _onAttach = (Control parentControl) => { };
+		private Action<Control> _onAddChild = (Control childControl) => { };
+		private Action _onFocus = () => { };
+		private Action _onUnfocus = () => { };
+		private Action _onUpdate = () => { };
+		private Action<SpriteBatch> _onDraw = (SpriteBatch spriteBatch) => { };
+
 		internal Control() { }
-		/*
-		public Control(Control parentControl) //public? internal?
+
+		public Control this[string name]
 		{
-			Parent = parentControl;
-			UI = Parent.UI;
-			Children = _children.AsReadOnly();
+			get => _children.Where(c => c.Name == name).FirstOrDefault();
 		}
-		*/
-		public Control AddControl<T>(string name = null) where T : Control, new()
+
+		public Control AddControl<T>(string name = null) where T : Control, new() => AddControl<T>(name, out T control);
+
+		public Control AddControl<T>(out T control) where T : Control, new() => AddControl<T>(null, out control);
+
+		public Control AddControl<T>(string name, out T control) where T : Control, new()
 		{
-			T control = new T()
-			{
-				Parent = this,
-				UI = this.UI
-			};
+			control = new T();
+
 			if (name != null || name != string.Empty)
 			{
 				control.Name = name;
 			}
+			_OnAddChild(control);
+			control._OnAttach(this);
 			return this;
 		}
 
@@ -38,6 +55,10 @@ namespace Ladybug.Beta.UI
 		public IList<Control> Children { get; private set; }
 
 		public UI UI { get; private set; }
+
+		public bool BlockCursor {get; set;} = true;
+
+		public Rectangle Bounds {get; private set;}
 
 		public int ZIndex
 		{
@@ -50,6 +71,112 @@ namespace Ladybug.Beta.UI
 					UI.RequestSort();
 				}
 			}
+		}
+
+		/// <summary>
+		/// Whether the control has been initialized
+		/// </summary>
+		/// <value></value>
+		public bool Initialized { get; private set; } = false;
+
+		/// <summary>
+		/// Sets the action performed when this control is initialized
+		/// </summary>
+		/// <param name="action"></param>
+		public Control OnInitialize(Action action)
+		{
+			_onInitialize = action;
+			return this;
+		}
+		internal void _OnInitialize()
+		{
+			if (!Initialized)
+			{
+				_onInitialize();
+				Initialized = true;
+			}
+		}
+
+		/// <summary>
+		/// Sets the action performed when this control is added as a child of another control
+		/// </summary>
+		/// <param name="action"></param>
+		public Control OnAttach(Action<Control> action)
+		{
+			_onAttach = action;
+			return this;
+		}
+		internal void _OnAttach(Control parentControl)
+		{
+			Parent = parentControl;
+			UI = parentControl.UI;
+			ZIndex = parentControl.ZIndex + 1;
+			_onAttach(parentControl);
+			_OnInitialize();
+		}
+
+		public Control OnAddChild(Action<Control> action)
+		{
+			_onAddChild = action;
+			return this;
+		}
+		internal void _OnAddChild(Control childControl)
+		{
+			if (!_children.Contains(childControl))
+			{
+				_children.Add(childControl);
+				_onAddChild(childControl);
+			}
+		}
+
+		public Control OnFocus(Action action)
+		{
+			_onFocus = action;
+			return this;
+		}
+		internal void _OnFocus()
+		{
+			_onFocus();
+			Focus?.Invoke(this, new EventArgs());
+		}
+
+		public Control OnUnfocus(Action action)
+		{
+			_onUnfocus = action;
+			return this;
+		}
+		internal void _OnUnfocus()
+		{
+			_onUnfocus();
+			Unfocus.Invoke(this, new EventArgs());
+		}
+
+		/// <summary>
+		/// Sets the action performed when this control is updated
+		/// </summary>
+		/// <param name="action"></param>
+		public Control OnUpdate(Action action)
+		{
+			_onUpdate = action;
+			return this;
+		} 
+		internal void _OnUpdate()
+		{
+			_onUpdate();
+		}
+
+		/// <summary>
+		///  Sets the action performed when this control is drawn
+		/// </summary>
+		/// <param name="action"></param>
+		public Control OnDraw(Action<SpriteBatch> action)
+		{
+			_onDraw = action;
+			return this;
+		}
+		internal void _OnDraw(SpriteBatch spriteBatch)
+		{
+			_onDraw(spriteBatch);
 		}
 	}
 }
--- src/beta/modules/ui/UI.cs
+++ src/beta/modules/ui/UI.cs
@@ -1,6 +1,11 @@
 using System;
 using System.Collections.Generic;
 
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using Ladybug.UserInput;
+
 namespace Ladybug.Beta.UI
 {
 	public class UI : Panel
@@ -15,12 +20,16 @@ namespace Ladybug.Beta.UI
 		{
 			Scene = scene;
 			Controls = _controls.AsReadOnly();
+			_OnInitialize();
+			OnAddChild(AddChild);
 		}
 
+		public Control FocusedControl { get; private set; }
+
 		public IList<Control> Controls { get; private set; }
-		
+
 		public Scene Scene { get; private set; }
-		
+
 		public VRC VRC { get; set; }
 
 		public void RequestSort()
@@ -28,17 +37,70 @@ namespace Ladybug.Beta.UI
 			_sortRequired = true;
 		}
 
+		private Vector2 GetCursorPosition()
+		{
+			var res = Input.Mouse.GetCursorPosition();
+
+			if (VRC != null)
+			{
+				res = VRC.ScreenToCanvasSpace(res);
+			}
+
+			return res;
+		}
+
+		private void AddChild(Control control)
+		{
+			_controls.Add(control);
+			_controlsByPriority.Add(control);
+			RequestSort();
+		}
+
+		private void CheckInput()
+		{
+			for (var i = 0; i < _controlsByPriority.Count; i++)
+			{
+				var control = _controlsByPriority[i];
+				if (control.Bounds.Contains(GetCursorPosition()))
+				{
+					//_control._OnCursorOver(); //we'll use this for CursorEnter/CursorLeave
+					// actually, we can probably do this in Control.cs in the update loop better.
+					if (control.BlockCursor)
+					{
+						break;
+					}
+				}
+			}
+		}
+
 		public void Update()
+		{
+			CheckInput();
+			_OnUpdate();
+			for (var i = 0; i < Controls.Count; i++)
+			{
+				Controls[i]._OnUpdate();
+			}
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
 		{
 			if (_sortRequired)
 			{
-				_controls.Sort((Control x, Control y) => 
+				_controlsByPriority.Sort((Control x, Control y) =>
 				{
 					var res = 0;
-					if (x.ZIndex > y.ZIndex) res = 1;
-					if (x.ZIndex < y.ZIndex) res = 1;
+					if (x.ZIndex > y.ZIndex) res = 1; //todo: verify correct </> values
+					if (x.ZIndex < y.ZIndex) res = -1;
 					return res;
 				});
+			}
+
+			_OnDraw(spriteBatch);
+
+			for (var i = 0; i < Controls.Count; i++)
+			{
+				Controls[i]._OnDraw(spriteBatch);
 			}
 		}
 	}
