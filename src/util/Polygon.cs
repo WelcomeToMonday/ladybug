b6c09ea35d8f26f675d396c3790ad337581f9592
Max Williams <max@welcometomonday.com>
2022-06-12 14:57:15 -0700

    Added polygon vertex sorting method

--- src/util/Polygon.cs
+++ src/util/Polygon.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using Microsoft.Xna.Framework;
 
@@ -21,12 +23,7 @@ namespace Ladybug
 			}
 			else
 			{
-				Points = new[]
-				{
-					Vector2.Zero,
-					Vector2.Zero,
-					Vector2.Zero
-				};
+				throw new ArgumentException("A polygon must consist of at least three vectors");
 			}
 		}
 
@@ -37,12 +34,37 @@ namespace Ladybug
 		public Vector2[] Points { get; protected set; }
 
 		/// <summary>
+		/// The mathematical center of the polygon, considered the average position of all of its points
+		/// </summary>
+		public Vector2 Centroid
+		{
+			get
+			{
+				float x = 0.0f;
+				float y = 0.0f;
+
+				foreach (var p in Points)
+				{
+					x += p.X;
+					y += p.Y;
+				}
+
+				x = x / Points.Length;
+				y = y / Points.Length;
+
+				return new Vector2(x, y);
+			}
+		}
+
+		/// <summary>
 		/// Returns <c>true</c> if the given vector lies within the bounds of the polygon
 		/// </summary>
 		/// <param name="vector"></param>
 		/// <returns></returns>
-		public bool ContainsVector(Vector2 vector)
+		public virtual bool ContainsVector(Vector2 vector)
 		{
+			// todo: replace w/ raycast algo
+			// - may want to save this somewhere as it is handy for any convex poly
 			bool res = false;
 
 			int j = Points.Length - 1;
@@ -58,6 +80,75 @@ namespace Ladybug
 				j = i;
 			}
 			return res;
+		}
+
+		/// <summary>
+		/// Sorts the Polygon's vertices clockwise around its centroid.
+		/// </summary>
+		/// <remarks>
+		/// As this uses the centroid of the polygon, the more irregular the polygon, the
+		/// less likely sorting it will result in an accurate shape. It is advised to construct highly-irregular
+		/// polygons from an already-sorted list of points rather than sorting them afterward.
+		/// </remarks>
+		public void SortPoints()
+		{
+			Func<Vector2, Vector2, float> getAngle = (c, v) =>
+			{
+				return MathF.Atan2(v.Y - c.Y, v.X - c.X);
+			};
+
+			Func<Vector2, Vector2, Vector2, int> resolveEqual = (c, x, y) =>
+			{
+				var vx = new Vector2(
+					MathF.Abs(c.X - x.X),
+					MathF.Abs(c.Y - x.Y)
+					);
+
+				var vy = new Vector2(
+					MathF.Abs(c.X - y.X),
+					MathF.Abs(c.Y - y.Y)
+					);
+
+				var diffX = vx.X + vx.Y;
+				var diffY = vy.X + vy.Y;
+
+				if (diffX > diffY)
+				{
+					return 1;
+				}
+				else if (diffX < diffY)
+				{
+					return -1;
+				}
+				else
+				{
+					return 0;
+				}
+			};
+
+			var c = Centroid;
+			var p = Points.ToList();
+
+			p.Sort((x, y) =>
+				{
+					var ax = getAngle(c, x);
+					var ay = getAngle(c, y);
+
+					if (ax > ay)
+					{
+						return 1;
+					}
+					else if (ax < ay)
+					{
+						return -1;
+					}
+					else
+					{
+						return resolveEqual(c, x, y);
+					}
+				});
+
+			Points = p.ToArray();
 		}
 	}
 }
